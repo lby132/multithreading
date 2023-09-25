248d5869ae6e3e2e11b95906a0dffae6dc04bb71
lby132 <quddbscjswow@naver.com>
2023-09-26 00:31:11 +0900

    thread

---
+++ src/interrupt/Main.java
@@ -0,0 +1,25 @@
+package interrupt;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final Thread thread = new Thread(new BlockingTask());
+
+        thread.start();
+
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(500000);
+            } catch (InterruptedException e) {
+                System.out.println("Exiting blocking thread");
+            }
+        }
+    }
+}
---
+++ src/interrupt/Main2.java
@@ -0,0 +1,44 @@
+package interrupt;
+
+import java.math.BigInteger;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("200000"), new BigInteger("1000000000"))); // 큰 숫자를 넣고 실행하면 계산에 오랜시간이 걸려 LongComputationTask 함수가 interrupt를해도 멈추지 않는다.
+
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class LongComputationTask implements Runnable {
+
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base + "^" + power + " = " + pow(base, power));
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                //코드 내에서 시간이 오래걸리는 곳에 인터럽트를 당했으면 계산을 멈추고 스레드를 종료하는 코드를 작성한다.
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Prematurely interrupted computation");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+
+            return result;
+        }
+    }
+}
