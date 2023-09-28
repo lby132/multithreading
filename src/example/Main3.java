bdbbf50af1970701527bd076075543ce861a847e
pro <quddbscjswow@naver.com>
2023-09-28 09:54:52 +0900

    Thread

--- src/example/Main3.java
+++ src/example/Main3.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main3 {
-
     public static void main(String[] args) {
         List<Long> inputNumbers = Arrays.asList(0L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
 
